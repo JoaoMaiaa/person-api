54aae8ec60b26bcb96fc43210323f7ed672f88f1
João Maia <15joaomaia@gmail.com>
2022-03-25 17:35:45 -0300

    acessando os dados e exibindo-os

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,24 @@ import { useState, useEffect } from 'react'
 import api from './services/api'
 
 function App() {
-  const [film, setFilm] = useState('')
+  const [film, setFilm] = useState([ ])
 
   useEffect(()=>{
     getFilm()
   }, [])
 
   async function getFilm(){
+    // response.data
     let { data } = await api.get('/')
     setFilm(data)
-    console.log(data)
+    console.log(film)
   }
 
   return (
     <div>
-      
+      {film.map(e=>(
+        <p>{e.name}</p>
+      ))}
     </div>
   )
 }
